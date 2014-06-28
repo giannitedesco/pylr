5833300033ccfdf46adc99485deed009f5a294cd
Gianni Tedesco <gianni@scaramanga.co.uk>
2014-06-28 17:48:23 +0100

    Add makefile and C test harness

---
+++ Makefile
@@ -0,0 +1,60 @@
+.SUFFIXES:
+
+CC := $(CROSS_COMPILE)gcc
+LD := $(CROSS_COMPILE)ld
+AR := $(CROSS_COMPILE)ar
+
+EXTRA_DEFS := -D_FILE_OFFSET_BITS=64
+CFLAGS := -g -pipe -O2 -Wall \
+	-flto -fwhole-program -mtune=corei7 \
+	-Wsign-compare -Wcast-align \
+	-Wstrict-prototypes \
+	-Wmissing-prototypes \
+	-Wmissing-declarations \
+	-Wmissing-noreturn \
+	-finline-functions \
+	-Wmissing-format-attribute \
+	-Wno-cast-align \
+	-fwrapv \
+	-Iinclude \
+	$(EXTRA_DEFS) 
+
+TEST_BIN := test
+TEST_LIBS := 
+TEST_OBJ = test.o
+
+LEX_TBL := lex.c
+
+ALL_BIN := $(TEST_BIN)
+ALL_OBJ := $(TEST_OBJ)
+ALL_DEP := $(patsubst %.o, .%.d, $(ALL_OBJ))
+ALL_TARGETS := $(ALL_BIN)
+
+TARGET: all
+
+.PHONY: all clean
+
+all: $(ALL_BIN)
+
+ifeq ($(filter clean, $(MAKECMDGOALS)),clean)
+CLEAN_DEP := clean
+else
+CLEAN_DEP :=
+endif
+
+%.o %.d: %.c $(CLEAN_DEP) $(ROOT_DEP) $(CONFIG_MAK) Makefile
+	@echo " [C] $<"
+	@$(CC) $(CFLAGS) -MMD -MF $(patsubst %.o, .%.d, $@) \
+		-MT $(patsubst .%.d, %.o, $@) \
+		-c -o $(patsubst .%.d, %.o, $@) $<
+
+$(TEST_BIN): $(TEST_OBJ)
+	@echo " [LINK] $@"
+	@$(CC) $(CFLAGS) -o $@ $(TEST_OBJ) $(TEST_LIBS)
+
+clean:
+	rm -f $(ALL_TARGETS) $(GRAMMAR) $(ALL_OBJ) $(ALL_DEP) tagops.c
+
+ifneq ($(MAKECMDGOALS),clean)
+-include $(ALL_DEP)
+endif
---
+++ test.c
@@ -0,0 +1,197 @@
+#include "lex.c"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#define BUF_INCREMENT 128
+typedef struct _tok *tok_t;
+typedef int(*token_cb)(tok_t tok, void *priv);
+typedef struct _lexer *lexer_t;
+
+lexer_t lexer_new(const char *name, token_cb cb, void *priv);
+int lexer_feed(lexer_t lex, char *buf, size_t len);
+int lexer_eof(lexer_t lex);
+void lexer_free(lexer_t lex);
+
+struct _lexer {
+	const char *l_name;
+	token_cb l_cb;
+	void *l_priv;
+	char *l_buf;
+	size_t l_len;
+	unsigned int l_state;
+	unsigned int l_line;
+	unsigned int l_col;
+};
+
+lexer_t lexer_new(const char *name, token_cb cb, void *priv)
+{
+	struct _lexer *l;
+
+	l = calloc(1, sizeof(*l));
+	if ( NULL == l )
+		goto out;
+
+	l->l_name = name;
+	l->l_cb = cb;
+	l->l_priv = priv;
+	l->l_state = initial_state;
+	l->l_line++;
+out:
+	return l;
+}
+
+static int to_buf(lexer_t l, char sym)
+{
+	if ( !(l->l_len % BUF_INCREMENT) ) {
+		char *new;
+		new = realloc(l->l_buf, l->l_len + BUF_INCREMENT);
+		if ( NULL== new )
+			return 0;
+		l->l_buf = new;
+		//DEBUG("upped buffer to %zu bytes", l->l_len + BUF_INCREMENT);
+	}
+	l->l_buf[l->l_len++] = sym;
+	return 1;
+}
+
+static void clear_buf(lexer_t l)
+{
+	free(l->l_buf);
+	l->l_buf = NULL;
+	l->l_len = 0;
+}
+
+static int lexer_symbol(lexer_t l, char sym)
+{
+	unsigned int old, new;
+
+	if ( sym == '\n' ) {
+		l->l_line++;
+		l->l_col = 0;
+	}else{
+		l->l_col++;
+	}
+
+again:
+	old = l->l_state;
+	new = trans[old][(uint8_t)sym];
+	if ( old && state[old].accept && (!new || !state[new].accept) ) {
+		//printf("END TOKEN\n\n");
+		printf("token: '%.*s'\n", (int)l->l_len, l->l_buf);
+		clear_buf(l);
+	}
+	if ( !new && old != initial_state ) {
+		l->l_state = initial_state;
+		goto again;
+	}
+
+	if ( old == initial_state && new ) {
+		//printf("BEGIN TOKEN\n");
+		clear_buf(l);
+	}
+	to_buf(l, sym);
+
+#if 0
+	if ( sym == '\n' ) {
+		printf("symbol is '\\n' : ");
+	}else if ( sym == '\t' ) {
+		printf("symbol is '\\t' : ");
+	}else{
+		printf("symbol is '%c'  : ", sym);
+	}
+
+	printf("%u (accept=%u reject=%u) -> "
+		"%u (accept=%u reject=%u)\n",
+		old, state[old].accept, old == 0,
+		new, state[new].accept, new == 0);
+#endif
+
+	l->l_state = new;
+	if ( !l->l_state )
+		l->l_state = initial_state;
+
+	return 1;
+}
+
+int lexer_feed(lexer_t l, char *buf, size_t len)
+{
+	size_t i;
+	for(i = 0; i < len; i++) {
+		if ( !lexer_symbol(l, buf[i]) )
+			return 0;
+	}
+	return 1;
+}
+
+int lexer_eof(lexer_t l)
+{
+	return lexer_symbol(l, '\n');
+}
+
+void lexer_free(lexer_t l)
+{
+	if ( l ) {
+		free(l->l_buf);
+		free(l);
+	}
+}
+
+static int tok_cb(tok_t tok, void *priv)
+{
+	return 1;
+}
+
+static int lex_file(const char *fn)
+{
+	lexer_t lex;
+	char buf[8192];
+	FILE *f;
+	size_t sz;
+	int ret = 0;
+
+	printf("opening %s\n", fn);
+
+	lex = lexer_new(fn, tok_cb, NULL);
+	if ( NULL == lex )
+		goto out;
+
+	f = fopen(fn, "r");
+	if ( NULL == f ) {
+		fprintf(stderr, "open: %s: %s", fn, strerror(errno));
+		goto out_free;
+	}
+
+	while((sz = fread(buf, 1, sizeof(buf), f))) {
+		if  (!lexer_feed(lex, buf, sz)) {
+			goto out_close;
+		}
+	}
+
+	if ( ferror(f) || !lexer_eof(lex) ) {
+		fprintf(stderr, "lex: %s: %s", fn, strerror(errno));
+		goto out_close;
+	}
+
+	/* success */
+	printf("OK\n");
+	ret = 1;
+
+out_close:
+	fclose(f);
+out_free:
+	lexer_free(lex);
+out:
+	return ret;
+}
+int main(int argc, char **argv)
+{
+	int i;
+
+	for(i = 1; i < argc; i++) {
+		if ( !lex_file(argv[i]) )
+			printf("%s: fucked\n", argv[i]);
+	}
+	return 0;
+}
